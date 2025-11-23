0aba6bb5ee3f7e7a182b897c0778cec517801883
Hariom Kumar Patel <109885070+hariomkumarpatel@users.noreply.github.com>
2025-11-24 01:26:43 +0530

    Initialize Firebase with configuration settings

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "YOUR_KEY",
+    authDomain: "YOUR_APP.firebaseapp.com",
+    databaseURL: "YOUR_URL",
+    projectId: "YOUR_ID",
+    storageBucket: "YOUR_BUCKET",
+};
+
+firebase.initializeApp(firebaseConfig);
